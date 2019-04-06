eaded0960d390f72c769b3ec2d26e72bae3c3f0a
Aakash Goel <goelaakash79@gmail.com>
2019-04-06 12:21:37 +0530

    models modified, dependencies added

--- models/Club.js
+++ models/Club.js
@@ -4,9 +4,9 @@ const ClubSchema = new mongoose.Schema({
     title: { type: String, required: true},
     desc: { type: String, required: true},
     logo: { type: String, required: true},
-    coverImage: ,
+    coverImage: String,
     numOfMembers: String,
-    lead: String
+    lead: { type: mongoose.Schema.Types.ObjectId, ref: "Srudent" }
 });
 
 module.exports = Club = mongoose.model('club', ClubSchema);
--- models/Student.js
+++ models/Student.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const StudentSchema = new mongoose.Schema({
     email: { type: String, required: true },
+    password: { type: String, required: true },
     name: String,
     role: String,
     phone: String,
